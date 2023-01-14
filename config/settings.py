b386ea7e69f8920385a4a77c28e1b6af40a1726d
sergey_klimov <s.klimov@bk.ru>
2023-01-14 22:47:35 +0700

    В проект добавлены настройки CELERY

--- config/settings.py
+++ config/settings.py
@@ -124,3 +124,18 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# CELERY
+CELERY_BROKER_URL = 'redis://127.0.0.1:6379/0'
+CELERY_TASK_TRACK_STARTED = True
+
+# Планировщик задач
+CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
+
+CELERY_BROKER_TRANSPORT_OPTION = {'visibility_timeout': 3600}
+CELERY_RESULT_BACKEND = 'django-db'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+
+CELERY_TASK_DEFAULT_QUEUE = 'default'
