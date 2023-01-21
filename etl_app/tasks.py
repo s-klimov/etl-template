cb63c9e2f60a91c32a3e40ac36a1972e05b9d218
sergey_klimov <s.klimov@bk.ru>
2023-01-21 22:56:41 +0700

    задача возвращает результат

--- etl_app/tasks.py
+++ etl_app/tasks.py
@@ -4,7 +4,9 @@ from etl_app import etl
 
 
 @shared_task(name="Задача ETL")
-def etl_task():
+def etl_task(*args, **kwargs):
     unloads = etl.load()
     multiplication = etl.transform(unloads)
     etl.extract(multiplication)
+
+    return "my result data"  # здесь может быть более полезная информация
