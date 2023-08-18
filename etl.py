c58f51cdb57bff41c65a653b74b5549217dc6b27
Sergey Klimov <ksiu@pkzdrav.ru>
2023-08-18 15:31:35 +0700

    implemented competitive task launch

--- etl.py
+++ etl.py
@@ -1,63 +1,69 @@
-from collections.abc import Generator
 from functools import wraps
-from typing import Tuple, Dict, Any
+from typing import Any, AsyncGenerator, Tuple
 
-import psycopg2
-from psycopg2.extras import DictCursor, DictRow
+import anyio
+import asyncpg
+from anyio.abc import TaskGroup
 
 SQL = """select id, number from etl.source"""
 
 
 def coroutine(func):
     @wraps(func)
-    def inner(*args:tuple[Any, ...], **kwargs: dict[str, Any]) -> Generator:
-        fn: Generator = func(*args, **kwargs)
-        next(fn)
+    async def inner(*args: tuple[Any, ...], **kwargs: dict[str, Any]) -> AsyncGenerator:
+        fn: AsyncGenerator = func(*args, **kwargs)
+        await anext(fn)
         return fn
 
     return inner
 
 
-def extract(batch: Generator) -> None:
-    """ Извлекает из БД строки и передает их в генератор
+async def extract(batch: AsyncGenerator) -> None:
+    """Извлекает из БД строки и передает их в генератор
 
     Args:
         batch: функция-генератор, в которую мы передаем значение
 
     """
 
-    dbs: Dict = dict(dbname='demo', user='sergei', password='sergei', host='localhost')
-    with psycopg2.connect(**dbs) as connection:
-        with connection.cursor(cursor_factory=DictCursor) as cursor:
-            cursor.execute(SQL)
-            record = cursor.fetchone()  # можно использовать fetchmany, чтобы извлекать данные "пачками"
-            while record:
-                batch.send(record)  # следим за тем, чтобы аргументом был итерируемый объект
-                record = cursor.fetchone()
+    conn = await asyncpg.connect(
+        database="demo", user="sergei", password="sergei", host="localhost"
+    )
+
+    stmt = await conn.prepare(SQL)
+    async with conn.transaction():
+        async for record in stmt.cursor():
+            await batch.asend(
+                record
+            )  # следим за тем, чтобы аргументом был итерируемый объект
+
+    await conn.close()
 
 
 @coroutine
-def transform(batch: Generator) -> Generator[None, DictRow, None]:
-
-    foo: int | str  # инструкция для mypy
-
-    while record := (yield):
-
+async def transform(batch: AsyncGenerator, task_group: TaskGroup) -> AsyncGenerator:
+    async def process(record):
+        await anyio.sleep(2)
         new_number = record["number"] ** 2
         if record["number"] % 2 == 0:
             foo = "an even number"
         elif record["number"] == 3:
             print("skip load stage")
-            continue
+            return
         else:
             foo = 0
+        await batch.asend((new_number, foo))
 
-        batch.send((new_number, foo))
+    while record := (yield):
+        print(record)
+        task_group.start_soon(process, record)
 
 
 @coroutine
-def load() -> Generator[None, Tuple, None]:
-    while subject := (yield):
+async def load(task_group: TaskGroup) -> AsyncGenerator:
+    await anyio.sleep(0)
+
+    async def process(subject: Tuple) -> None:
         match subject:
             case (int(number), str(bar)):
                 print("the square of", bar, number)
@@ -65,3 +71,6 @@ def load() -> Generator[None, Tuple, None]:
                 print(number)
             case _:
                 raise SyntaxError(f"Unknown structure of {subject=}")
+
+    while subject := (yield):
+        task_group.start_soon(process, subject)
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+from anyio import create_task_group, run
+
 import etl
 
 
-def main():
-    unloads = etl.load()
-    multiplication = etl.transform(unloads)
-    etl.extract(multiplication)
+async def main():
+    async with create_task_group() as tg:
+        unloads = await etl.load(tg)
+        multiplication = await etl.transform(unloads, tg)
+        await etl.extract(multiplication)
 
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    run(main)
